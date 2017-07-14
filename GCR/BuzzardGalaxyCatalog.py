3475ecf3f26baef626aa8f8e630bc622a921f7ad
Yao-Yuan Mao <yymao.astro@gmail.com>
2017-07-14 09:10:05 -0700

    update BuzzardGalaxyCatalog

--- GCR/BuzzardGalaxyCatalog.py
+++ GCR/BuzzardGalaxyCatalog.py
@@ -1,7 +1,7 @@
 """
 Buzzard galaxy catalog class.
 """
-from __future__ import division
+from __future__ import division, print_function
 import os
 import numpy as np
 from astropy.io import fits
@@ -21,18 +21,31 @@ class BuzzardGalaxyCatalog(BaseGalaxyCatalog):
     defined by BaseGalaxyCatalog class.
     """
 
-    def _subclass_init(self, catalog_dir, base_catalog_dir=os.curdir, **kwargs):
+    def _subclass_init(self, 
+                       catalog_dir, 
+                       base_catalog_dir=os.curdir, 
+                       cosmo_h=0.7, 
+                       cosmo_Omega_M0=0.286,
+                       npix=768,
+                       catalog_subdirs=('truth',),
+                       filename_template='Chinchilla-0_lensed.{}.fits',
+                       **kwargs):
 
         self._pre_filter_quantities = {'original_healpixel'}
+        self.cosmology = FlatLambdaCDM(H0=cosmo_h*100.0, Om0=cosmo_Omega_M0)
 
+        _c = 299792.458
         self._quantity_modifiers = {
             'galaxy_id': ('truth', 'ID'),
+            'redshift': (lambda zt, x, y, z, vx, vy, vz: zt + (x*vx+y*vy+z*vz)/np.sqrt(x*x+y*y+z*z)/_c, 
+                ('truth', 'Z'), ('truth', 'PX'), ('truth', 'PY'), ('truth', 'PZ'), ('truth', 'VX'), ('truth', 'VY'), ('truth', 'VZ')),
             'redshift_true': ('truth', 'Z'),
             'ra': ('truth', 'RA'),
             'dec': ('truth', 'DEC'),
             'ra_true': ('truth', 'TRA'),
             'dec_true': ('truth', 'TDEC'),
             'halo_id': ('truth', 'HALOID'),
+            'halo_mass': (lambda x: x/self.cosmology.h, ('truth', 'M200')),
             'is_bcg': (lambda x: x.astype(np.bool), ('truth', 'CENTRAL')),
             'ellipticity_1': ('truth', 'EPSILON', 0),
             'ellipticity_2': ('truth', 'EPSILON', 1),
@@ -44,34 +57,33 @@ class BuzzardGalaxyCatalog(BaseGalaxyCatalog):
             'shear_2': ('truth', 'GAMMA2'),
             'convergence': ('truth', 'KAPPA'),
             'magnification': ('truth', 'MU'),
-            'position_x': ('truth', 'PX'),
-            'position_y': ('truth', 'PY'),
-            'position_z': ('truth', 'PZ'),
+            'position_x': (lambda x: x/self.cosmology.h, ('truth', 'PX')),
+            'position_y': (lambda x: x/self.cosmology.h, ('truth', 'PY')),
+            'position_z': (lambda x: x/self.cosmology.h, ('truth', 'PZ')),
             'velocity_x': ('truth', 'VX'),
             'velocity_y': ('truth', 'VY'),
             'velocity_z': ('truth', 'VZ'),
         }
 
+        _mask_func = lambda x: np.where(x==99.0, np.nan, x)
         for i, b in enumerate('grizY'):
-            self._quantity_modifiers['Mag01_true_{}_des'.format(b)] = ('truth', 'AMAG', i)
-            self._quantity_modifiers['Mag01_true_{}_any'.format(b)] = ('truth', 'AMAG', i)
-            self._quantity_modifiers['mag_{}_des'.format(b)] = ('truth', 'OMAG', i)
-            self._quantity_modifiers['mag_{}_any'.format(b)] = ('truth', 'OMAG', i)
-            self._quantity_modifiers['magerr_{}_des'.format(b)] = ('truth', 'OMAGERR', i)
-            self._quantity_modifiers['magerr_{}_any'.format(b)] = ('truth', 'OMAGERR', i)
-
+            self._quantity_modifiers['Mag01_true_{}_des'.format(b)] = (_mask_func, ('truth', 'AMAG', i))
+            self._quantity_modifiers['Mag01_true_{}_any'.format(b)] = (_mask_func, ('truth', 'AMAG', i))
+            self._quantity_modifiers['mag_{}_des'.format(b)] = (_mask_func, ('truth', 'OMAG', i))
+            self._quantity_modifiers['mag_{}_any'.format(b)] = (_mask_func, ('truth', 'OMAG', i))
+            self._quantity_modifiers['magerr_{}_des'.format(b)] = (_mask_func, ('truth', 'OMAGERR', i))
+            self._quantity_modifiers['magerr_{}_any'.format(b)] = (_mask_func, ('truth', 'OMAGERR', i))
 
         self._catalog_dir = os.path.join(base_catalog_dir, catalog_dir)
-        self._catalog_subdirs = ('truth',)
-        self.cosmology = FlatLambdaCDM(H0=0.7, Om0=0.286)
-        self._npix = 768
-        self._filename_template = 'Chinchilla-0_lensed.{}.fits'
+        self._catalog_subdirs = catalog_subdirs
+        self._npix = npix
+        self._filename_template = filename_template
 
 
     def _generate_native_quantity_list(self):
         native_quantities = {'original_healpixel'}
         for _, dataset in self._iter_native_dataset():
-            for k, v in dataset.iteritems():
+            for k, v in dataset.items():
                 fields = _get_fits_data(v).dtype.fields
                 for name, (dt, size) in _get_fits_data(v).dtype.fields.items():
                     if dt.shape:
@@ -99,7 +111,7 @@ class BuzzardGalaxyCatalog(BaseGalaxyCatalog):
                 if all(subdir in fp for subdir in self._catalog_subdirs):
                     yield i, fp
             finally:
-                for f in fp.itervalues():
+                for f in fp.values():
                     f.close()
 
 
