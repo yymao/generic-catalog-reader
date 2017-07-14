342198e52943361d931ddd0d544d5275052980bf
Yao-Yuan Mao <yymao.astro@gmail.com>
2017-07-14 13:58:41 -0400

    add load_yaml_config

--- GCR/__init__.py
+++ GCR/__init__.py
@@ -2,3 +2,10 @@ from .BaseGalaxyCatalog import BaseGalaxyCatalog
 from .GalacticusGalaxyCatalog import GalacticusGalaxyCatalog
 from .BuzzardGalaxyCatalog import BuzzardGalaxyCatalog
 from .AlphaQGalaxyCatalog import AlphaQGalaxyCatalog
+
+import yaml
+
+def load_yaml_config(filename):
+    with open(filename) as f:
+        config = yaml.load(f.read())
+    return config
