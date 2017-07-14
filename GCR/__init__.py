25e30a1c6e8b930602f0de575cd49e6bd7ba8b32
Yao-Yuan Mao <yymao.astro@gmail.com>
2017-07-14 14:11:50 -0400

    add load_catalog function

--- GCR/__init__.py
+++ GCR/__init__.py
@@ -5,7 +5,11 @@ from .AlphaQGalaxyCatalog import AlphaQGalaxyCatalog
 
 import yaml
 
-def load_yaml_config(filename):
+def load_yaml_config(yaml_config_file):
     with open(filename) as f:
         config = yaml.load(f.read())
     return config
+
+def load_catalog(yaml_config_file)
+    config = load_yaml_config(yaml_config_file)
+    return locals()[config['subclass_name']](**config)
