cf17c59b35313aecb0c07fe251519c108b2ca477
Yao-Yuan Mao <yymao.astro@gmail.com>
2017-07-17 13:18:12 -0400

    add config_overwrite to load_catalog

--- GCR/__init__.py
+++ GCR/__init__.py
@@ -5,11 +5,27 @@ from .AlphaQGalaxyCatalog import AlphaQGalaxyCatalog
 
 import yaml
 
-def load_yaml_config(yaml_config_file):
+def _load_yaml_config(yaml_config_file):
     with open(yaml_config_file) as f:
         config = yaml.load(f.read())
     return config
 
-def load_catalog(yaml_config_file):
-    config = load_yaml_config(yaml_config_file)
+def load_catalog(yaml_config_file, config_overwrite=None):
+    """
+    Load a galaxy catalog as specified in a yaml config file.
+
+    Parameters
+    ----------
+    yaml_config_file : str
+        path to the yaml config file
+    config_overwrite : dict, optional
+        a dictionary of config options to overwrite
+
+    Return
+    ------
+    galaxy_catalog : subclass of BaseGalaxyCatalog
+    """
+    config = _load_yaml_config(yaml_config_file)
+    if config_overwrite:
+        config.update(config_overwrite)
     return globals()[config['subclass_name']](**config)
