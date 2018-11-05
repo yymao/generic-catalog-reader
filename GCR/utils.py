045348a7d5d1d8f3f84bd4568711282179a4a4cf
Yao-Yuan Mao <yymao.astro@gmail.com>
2018-11-05 11:38:22 -0500

    add concatenate_1d

--- GCR/utils.py
+++ GCR/utils.py
@@ -4,7 +4,7 @@ Utility module
 import numpy as np
 from numpy.core.records import fromarrays
 
-__all__ = ['dict_to_numpy_array']
+__all__ = ['is_string_like', 'trivial_callable', 'dict_to_numpy_array', 'concatenate_1d']
 
 def is_string_like(obj):
     """
@@ -25,3 +25,10 @@ def dict_to_numpy_array(d):
     Convert a dict of 1d array to a numpy recarray
     """
     return fromarrays(d.values(), np.dtype([(str(k), v.dtype) for k, v in d.items()]))
+
+def concatenate_1d(arrays):
+    if len(arrays) == 0:
+        return np.array([])
+    if len(arrays) == 1:
+        return np.asarray(arrays[0])
+    return np.concatenate(arrays)
