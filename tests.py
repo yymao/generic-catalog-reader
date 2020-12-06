ea9453740d09a1c346eec14de8a9d369907cf604
Yao-Yuan Mao <yymao.astro@gmail.com>
2020-12-06 11:16:38 -0500

    remove trivial tests

--- tests.py
+++
@@ -1,7 +0,0 @@
-from GCR import *
-
-def test_trivial():
-    assert callable(dict_to_numpy_array)
-    assert callable(GCRQuery)
-    assert callable(BaseGenericCatalog)
-    assert callable(CompositeCatalog)
