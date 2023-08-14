141a87fc394d0024e437c07d6bedeab73af56784
Yao-Yuan Mao <yymao.astro@gmail.com>
2023-08-14 16:45:39 -0600

    minor code formatting issues

--- GCR/composite.py
+++ GCR/composite.py
@@ -357,12 +357,11 @@ class CompositeCatalog(BaseGenericCatalog):
             # match column if needed:
 
             if cat.matching_by_column:
-
                 selector = None if cat.non_unique_match_selector is None else cat.cache[cat.non_unique_match_selector]
                 matching_idx, not_matched_mask, cat.sorter = _match(
                     cat.cache[cat.matching_by_column],
                     data[(self._main.identifier, cat.matching_column_in_main)],
-                    cat.sorter, selector, cat.select_descending
+                    cat.sorter, selector, cat.select_descending,
                 )
 
                 for q, v in _slice_matched(
